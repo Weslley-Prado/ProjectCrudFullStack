df1b99c544844c0b0cd342647c7feea6730cfe8d
Weslley Prado <weslley.prado73@gmail.com>
2022-03-11 23:20:16 -0300

    Update scripts

---
+++ Frontend/rld-registerleveldeveloper/src/components/templates/Footer.jsx
@@ -0,0 +1,8 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+
+    <footer className="footer">
+        Desenvolvido com por <strong> Weslley Prado</strong>
+    </footer>
