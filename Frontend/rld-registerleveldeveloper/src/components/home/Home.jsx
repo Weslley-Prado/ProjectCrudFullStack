a197cb1c8e08ec0550d1258717dc8ea41d6706e1
Weslley Prado <weslley.prado73@gmail.com>
2022-03-11 23:14:29 -0300

    Update scripts

---
+++ Frontend/rld-registerleveldeveloper/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../templates/Main'
+
+export default props =>
+    <Main icon="home" title="Inicio"
+        subtitle="Projeto React">
+        <div className="display-4">Welcome</div>
+        <hr />
+        <p className="mb-0">In development</p>
+
+    </Main>
--- Frontend/rld-registerleveldeveloper/src/main/App.jsx
+++ Frontend/rld-registerleveldeveloper/src/main/App.jsx
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import './App.css'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Logo from '../components/templates/Logo'
+import Nav from '../components/templates/Nav'
+import Routes from './Routes'
+import Footer from '../components/templates/Footer'
+
+
+export default props =>
+<BrowserRouter>
+<div className="app">
+    <Logo />
+    <Nav />
+    <Routes />
+    <Footer />
     </div>
-  );
-}
 
-export default App;
+</BrowserRouter>
